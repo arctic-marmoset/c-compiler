cd29c5bcd0fffeda984be41d26c9ef61066dd69a
ArcticMarmoset <45583929+ArcticMarmoset@users.noreply.github.com>
2020-07-01 22:25:23 -0400

    Replace C-style strings with string_view

--- CCompiler/src/definitions.h
+++ CCompiler/src/definitions.h
@@ -1,29 +1,31 @@
 #ifndef C_COMPILER_DEFINITIONS_H
 #define C_COMPILER_DEFINITIONS_H
 
+#include <string_view>
+
 namespace keyworddefs
 {
-    inline constexpr const char *char_kw   = "char";
-    inline constexpr const char *int_kw    = "int";
-    inline constexpr const char *double_kw = "double";
-    inline constexpr const char *float_kw  = "float";
-    inline constexpr const char *struct_kw = "struct";
-    inline constexpr const char *enum_kw   = "enum";
-    inline constexpr const char *void_kw   = "void";
-    inline constexpr const char *short_kw  = "short";
-    inline constexpr const char *long_kw   = "long";
+    inline constexpr std::string_view char_kw   = "char";
+    inline constexpr std::string_view int_kw    = "int";
+    inline constexpr std::string_view double_kw = "double";
+    inline constexpr std::string_view float_kw  = "float";
+    inline constexpr std::string_view struct_kw = "struct";
+    inline constexpr std::string_view enum_kw   = "enum";
+    inline constexpr std::string_view void_kw   = "void";
+    inline constexpr std::string_view short_kw  = "short";
+    inline constexpr std::string_view long_kw   = "long";
 
-    inline constexpr const char *const_kw  = "const";
-    inline constexpr const char *static_kw = "static";
+    inline constexpr std::string_view const_kw  = "const";
+    inline constexpr std::string_view static_kw = "static";
 
-    inline constexpr const char *if_kw       = "if";
-    inline constexpr const char *else_kw     = "else";
-    inline constexpr const char *for_kw      = "for";
-    inline constexpr const char *while_kw    = "while";
-    inline constexpr const char *break_kw    = "break";
-    inline constexpr const char *continue_kw = "continue";
+    inline constexpr std::string_view if_kw       = "if";
+    inline constexpr std::string_view else_kw     = "else";
+    inline constexpr std::string_view for_kw      = "for";
+    inline constexpr std::string_view while_kw    = "while";
+    inline constexpr std::string_view break_kw    = "break";
+    inline constexpr std::string_view continue_kw = "continue";
 
-    inline constexpr const char *return_kw = "return";
+    inline constexpr std::string_view return_kw = "return";
 }
 
 namespace chardefs
